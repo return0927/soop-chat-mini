2f6186faf9ae9cbeb0997ec5b936e8d61fb7d693
Eunhak Lee <lee@enak.kr>
2025-01-29 08:03:56 +0900

    feat: add Google Analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePluginRadar } from 'vite-plugin-radar';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: process.env.VITE_GOOGLE_ANALYTICS || '',
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     hmr: false,
     allowedHosts: ['soop-chat-mini.go9.ma'],
