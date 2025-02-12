f029c6c42c3fd865e04fed302c173c821c95a21e
Eunhak Lee <lee@enak.kr>
2025-02-13 02:27:40 +0900

    feat: implement signature emote displaying

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,27 @@ import SocketHolder from './components/SocketHolder.tsx';
 import { useEffect, useState } from 'react';
 import Marquee from 'react-fast-marquee';
 import { ChatMessage, StreamMeta } from './types/stream.ts';
+import { SignatureEmoticonResponse } from './types/api.ts';
+
+function arrayChoice<T>(array: Array<T>): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function getRandomId(length: number = 12) {
+  const CHARS = Array.from('0123456789abcdefghijklmnopqrstuvwxyz');
+  return Array.from(Array(length).keys())
+    .map(() => arrayChoice(CHARS))
+    .join('');
+}
+
+type MessageEntry = {
+  messages: JSX.Element[];
+  charLength: number;
+  position: string;
+  key: string;
+};
+
+type EmoteMap = { [key: string]: string };
 
 function App() {
   const params = new URL(window.location.href).searchParams;
@@ -17,11 +38,13 @@ function App() {
   const [errorMessage, setErrorMessage] = useState(
     '채널이 온라인이 되기를 기다리는 중...'
   );
-  const [messages, setMessages] = useState<string[][]>([]);
-  const [lastRandom, setLastRandom] = useState<number>(-1.0);
+  const [messages, setMessages] = useState<MessageEntry[]>([]);
   const setRefreshTimer = useState<NodeJS.Timeout | null>(null)[1];
   const [title, setTitle] = useState<string>('SOOP 미니 채팅');
+  const [emoteMap, setEmoteMap] = useState<EmoteMap>({});
+  const [emoteMapHolder] = useState<{ emoteMap: EmoteMap }>({ emoteMap });
 
+  let lastRandom = -1.0;
   const getBalancedRandom = (preset = Math.random()): number => {
     if (Math.abs(lastRandom - preset) < 0.1) {
       return Math.abs(lastRandom - (1 - preset)) < 0.1
@@ -29,11 +52,41 @@ function App() {
         : getBalancedRandom(1 - preset);
     }
 
-    setLastRandom(() => preset);
+    lastRandom = preset;
     return preset;
   };
   const handler = (message: ChatMessage) => {
-    setMessages((arr) => [...arr, [message.message, '' + getBalancedRandom()]]);
+    if (!message) {
+      console.log('Ignoring null message');
+      return;
+    }
+
+    let charLength = 0;
+    const messageParts = message.message
+      .split(/([^/\n]+)|(\/[^/]+\/)/)
+      .filter((v) => v)
+      .map((v) => {
+        const emoteId = v.slice(1, v.length - 1);
+        if (
+          !v.startsWith('/') ||
+          !v.endsWith('/') ||
+          !emoteMapHolder.emoteMap[emoteId]
+        ) {
+          charLength += v.length;
+          return <span>{v}</span>;
+        }
+
+        charLength += 1;
+        return <img src={emoteMapHolder.emoteMap[emoteId]} alt={emoteId} />;
+      });
+
+    const entry: MessageEntry = {
+      key: getRandomId(),
+      messages: messageParts,
+      charLength: charLength,
+      position: '' + getBalancedRandom(),
+    };
+    setMessages((arr) => [...arr, entry]);
   };
   const errorConsumer = (message: string) => {
     setErrorMessage(message);
@@ -41,6 +94,54 @@ function App() {
   const streamMetaHandler: (args: StreamMeta) => void = (args) => {
     setTitle(() => `SOOP 미니 채팅 - ${args.nick}(${args.id})`);
   };
+
+  useEffect(() => {
+    fetch(
+      `/proxy/api/signature_emoticon_api.php?v=tier&work=list&szBjId=${channelId}`
+    ).then(async (body) => {
+      const json: SignatureEmoticonResponse = await body.json();
+      const { img_path: baseUrl, data } = json;
+
+      const signatureEmoticons: string[][] = [];
+      Array.of(data['tier1'], data['tier2']).forEach((arr) => {
+        if (arr)
+          arr.forEach((entry) => {
+            signatureEmoticons.push([
+              entry.title,
+              new URL(entry.pc_img, baseUrl).href,
+            ]);
+          });
+      });
+      setEmoteMap((currentMap) => {
+        const newMap = { ...currentMap };
+        signatureEmoticons.forEach(([key, url]) => {
+          newMap[key] = url;
+        });
+        console.log(`New map: ${Object.keys(newMap)}`);
+        console.log(newMap);
+        return newMap;
+      });
+      console.log(`Updated emoteMap: ${signatureEmoticons.map((v) => v[0])}`);
+    });
+  }, [channelId]);
+
+  useEffect(() => {
+    setInterval(() => {
+      handler({
+        id: 'foobar1234',
+        nick: 'TestUser',
+        message:
+          '잘한다 ' +
+          arrayChoice(['/sesekki/', '/sesehi/', '/seheart/', '/segoo/']) +
+          arrayChoice(['/sesekki/', '/sesehi/', '/seheart/', '/segoo/']) +
+          '!!',
+      });
+    }, 100);
+  }, []);
+
+  useEffect(() => {
+    emoteMapHolder.emoteMap = emoteMap;
+  }, [emoteMap]);
 
   useEffect(() => {
     if (errorMessage) {
@@ -85,25 +186,23 @@ function App() {
       )}
 
       {messages &&
-        messages.map(([message, position]) => (
+        messages.map(({ key, messages, position, charLength }) => (
           <Marquee
-            key={`${message}-${position}`}
+            key={key}
             style={{
               position: 'fixed',
               top: `calc((100vh - 3em) * ${position} + 1em)`,
-              left: `-${message.length}em`,
-              width: `calc(100% + ${message.length}em)`,
+              left: `calc(-5 * ${charLength}em)`,
+              width: `calc(100% + 5 * ${charLength}em)`,
             }}
             delay={0.1}
             speed={scrollAmount + scrollOffset * (+position - 0.5)}
             loop={1}
             onFinish={() => {
-              setMessages((arr) =>
-                arr.filter((a) => a[0] !== message || a[1] !== position)
-              );
+              setMessages((arr) => arr.filter((a) => a.key !== key));
             }}
           >
-            {message}
+            {...messages}
           </Marquee>
         ))}
     </div>
--- src/types/api.ts
+++ src/types/api.ts
@@ -10,3 +10,26 @@ export type PlayerLiveApiResponse = {
     CATEGORY_TAGS: string[];
   };
 };
+
+export type SignatureEmoticonEntry = {
+  title: string;
+  tier_type: number;
+  pc_img: string;
+  mobile_img: string;
+  pc_alt_img?: string;
+  mobile_alt_img?: string;
+  move_img: 'Y' | 'N';
+  black_keyword: 'Y' | 'N';
+};
+
+export type SignatureEmoticonResponse = {
+  img_path: string;
+  black_version: string;
+  result: number;
+  tier_type: number;
+  version: number;
+  data: {
+    tier1?: SignatureEmoticonEntry[];
+    tier2?: SignatureEmoticonEntry[];
+  };
+};
